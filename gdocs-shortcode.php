eeb23728e7b1e67735e8d38d9a139e1f2db1d103
r-a-y <r-a-y@gmx.com>
2015-09-09 18:02:27 -0700

    Shortcake: Add 'pdf' as a selectable option for 'type' attribute.
    See #6.

--- gdocs-shortcode.php
+++ gdocs-shortcode.php
@@ -67,7 +67,8 @@ function ray_gdoc_shortcode_init() {
 						'options' => array(
 							'' => '--',
 							'audio' => __( 'Audio', 'google-docs-shortcode' ),
-							'image' => __( 'Image', 'google-docs-shortcode' )
+							'image' => __( 'Image', 'google-docs-shortcode' ),
+							'pdf'   => __( 'PDF', 'google-docs-shortcode' ),
 						),
 						'description' => __( "If your Google Drive item is not a Doc, Slide, Spreadsheet or Form, select the type of item you are embedding.", 'gdrive' )
 					),
