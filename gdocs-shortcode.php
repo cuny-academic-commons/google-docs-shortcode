05d415c4008553c227ae009a4c5aa8de6096a3e7
r-a-y <r-a-y@gmx.com>
2015-10-15 16:00:12 -0700

    Bump version to 0.5-bleeding.

--- gdocs-shortcode.php
+++ gdocs-shortcode.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/cuny-academic-commons/google-docs-shortcode
 Description: Easily embed a Google Doc into your blog posts
 Author: r-a-y
 Author URI: http://profiles.wordpress.org/r-a-y
-Version: 0.4
+Version: 0.5-bleeding
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
